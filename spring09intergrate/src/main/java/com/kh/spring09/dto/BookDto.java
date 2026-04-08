b8dadf61dd4c8ecf212642216276bbfa4aa765ac
alskdj1234 <qpalzm7714@gmail.com>
2026-04-08 14:52:36 +0900

    롬복

--- spring09intergrate/src/main/java/com/kh/spring09/dto/BookDto.java
+++ spring09intergrate/src/main/java/com/kh/spring09/dto/BookDto.java
@@ -1,5 +1,8 @@
 package com.kh.spring09.dto;
 
+import lombok.Data;
+
+@Data
 public class BookDto {
 	private int bookId;
 	private String bookTitle;
@@ -10,79 +13,6 @@ public class BookDto {
 	private int bookPageCount;
 	private String bookGenre;
 
-	public BookDto() {
-		super();
-	}
-
-	public int getBookId() {
-		return bookId;
-	}
-
-	public void setBookId(int bookId) {
-		this.bookId = bookId;
-	}
-
-	public String getBookTitle() {
-		return bookTitle;
-	}
-
-	public void setBookTitle(String bookTitle) {
-		this.bookTitle = bookTitle;
-	}
-
-	public String getBookAuthor() {
-		return bookAuthor;
-	}
-
-	public void setBookAuthor(String bookAuthor) {
-		this.bookAuthor = bookAuthor;
-	}
-
-	public String getBookPublicationDate() {
-		return bookPublicationDate;
-	}
-
-	public void setBookPublicationDate(String bookPublicationDate) {
-		this.bookPublicationDate = bookPublicationDate;
-	}
-
-	public long getBookPrice() {
-		return bookPrice;
-	}
-
-	public void setBookPrice(long bookPrice) {
-		this.bookPrice = bookPrice;
-	}
-
-	public String getBookPublisher() {
-		return bookPublisher;
-	}
-
-	public void setBookPublisher(String bookPublisher) {
-		this.bookPublisher = bookPublisher;
-	}
-
-	public int getBookPageCount() {
-		return bookPageCount;
-	}
-
-	public void setBookPageCount(int bookPageCount) {
-		this.bookPageCount = bookPageCount;
-	}
-
-	public String getBookGenre() {
-		return bookGenre;
-	}
-
-	public void setBookGenre(String bookGenre) {
-		this.bookGenre = bookGenre;
-	}
-
-	@Override
-	public String toString() {
-		return "BookDto [bookId=" + bookId + ", bookTitle=" + bookTitle + ", bookAuthor=" + bookAuthor
-				+ ", bookPublicationDate=" + bookPublicationDate + ", bookPrice=" + bookPrice + ", bookPublisher="
-				+ bookPublisher + ", bookPageCount=" + bookPageCount + ", bookGenre=" + bookGenre + "]";
-	}
+	//조건이나 가상 게터는 직접 해야됨
 
 }
--- spring09intergrate/src/main/java/com/kh/spring09/dto/CountryDto.java
+++ spring09intergrate/src/main/java/com/kh/spring09/dto/CountryDto.java
@@ -1,49 +1,26 @@
 package com.kh.spring09.dto;
-//등록 없이 필요할 때마다 만들어서 사용
+import lombok.Data;
+//lombok에서 제공하는 어노테이션으로 필드를 제외한 나머지 기본요소를 생성
+//이 라이브러리는 이클립스를 속여야 하는 라이브러리(추가 설치 작업이 필요)
+//프로젝트에만 적용하면 원하는 효과를 보기 어려움
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Setter//세터 메소드 생성
+//@Getter//게터 메소드 생성
+//@NoArgsConstructor//기본 생성자 생성
+//@ToString//투스트링 생성 ()로 옵션도 가능
+
+@Data//위에 4개 다 합침
 public class CountryDto {
+	
 private int countryNo;
 private String countryRegion;
 private String countryName;
 private String countryCapital;
 private long countryPopulation;
-public CountryDto() {
-	super();
-}
-@Override
-public String toString() {
-	return "CountryDto [countryNo=" + countryNo + ", countryRegion=" + countryRegion + ", countryName=" + countryName
-			+ ", countryCapital=" + countryCapital + ", countryPopulation=" + countryPopulation + "]";
-}
-public int getCountryNo() {
-	return countryNo;
-}
-public void setCountryNo(int countryNo) {
-	this.countryNo = countryNo;
-}
-public String getCountryRegion() {
-	return countryRegion;
-}
-public void setCountryRegion(String countryRegion) {
-	this.countryRegion = countryRegion;
-}
-public String getCountryName() {
-	return countryName;
-}
-public void setCountryName(String countryName) {
-	this.countryName = countryName;
-}
-public String getCountryCapital() {
-	return countryCapital;
-}
-public void setCountryCapital(String countryCapital) {
-	this.countryCapital = countryCapital;
-}
-public long getCountryPopulation() {
-	return countryPopulation;
-}
-public void setCountryPopulation(long countryPopulation) {
-	this.countryPopulation = countryPopulation;
-}
 
 
 
